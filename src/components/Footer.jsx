5c6519fa873a253f26a69d4cbcfcb5412fa76c0f
Ido <Idosabach11@gmail.com>
2024-10-10 00:17:24 +0300

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,28 @@
 export default function Footer() {
   return (
-    <footer className="text-sm flex flex-col items-center justify-center p-4 pl-0 pr-0">
+    <footer className="text-sm flex flex-col justify-start p-4 pl-0 pr-0">
       <hr className="w-full border-gray-300 mb-2" />
-      <section>
-        <p className="text-gray-600">Owned by Neria Abutbul 2025</p>
+      <section class="flex gap-6 justify-between pl-3 pr-3">
+        <section className="flex flex-row gap-1 items-center">
+          <img
+            width="24"
+            height="24"
+            src="https://img.icons8.com/material-outlined/24/copyright.png"
+            alt="copyright"
+            className="h-4 w-4"
+          />
+          <p className="text-gray-600">Owned by Neria Abutbul 2025</p>
+        </section>
+        <section className="flex flex-row gap-1 items-center">
+          <img
+            width="24"
+            height="24"
+            src="https://img.icons8.com/material-outlined/24/place-marker--v1.png"
+            alt="place-marker--v1"
+            className="h-4 w-4"
+          />
+          <p className="text-gray-600 font-bold">Be'er Sheva</p>
+        </section>
       </section>
     </footer>
   );
