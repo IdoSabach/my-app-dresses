cb4d1ced8a07835b4d265cd6595f272aa897a101
Ido <Idosabach11@gmail.com>
2024-10-12 20:56:30 +0300

    change video from 1.4MB => 700KB

--- src/components/componentsForHome/HeroSection.jsx
+++ src/components/componentsForHome/HeroSection.jsx
@@ -4,7 +4,7 @@ export default function HeroSection() {
   return (
     <section className="relative">
       <video autoPlay loop muted width="2500" className="z-0" controls={false} playsInline>
-        <source src="/video/vid.mp4" type="video/mp4" />
+        <source src="/video/vid1.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <h1 className="absolute top-0 left-0 text-black text-sm md:text-3xl font-medium p-4 z-10 flex flex-col md:gap-2">
